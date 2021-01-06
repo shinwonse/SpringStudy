0122ea0b22ad1b3ff6d339bd8ede88ccf6ec9031
wonse <sinwonse@gmail.com>
2021-01-06 16:39:42 +0900

    21.01.06

--- src/main/java/hello/hellospring/HelloSpringApplication.java
+++ src/main/java/hello/hellospring/HelloSpringApplication.java
@@ -7,6 +7,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class HelloSpringApplication {
 
 	public static void main(String[] args) {
+
+		
 		SpringApplication.run(HelloSpringApplication.class, args);
 	}
 
